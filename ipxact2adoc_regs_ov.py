544a2fd5286c3ac7e3451be08195de324f64edd9
Dmitry Murzinov <kakstattakim@gmail.com>
2017-04-03 01:32:20 +0300

    Ability to generate an AsciiDoc Register Overview table

---
+++ ipxact2adoc_regs_ov.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+sys.path.append('ipyxact')
+
+from ipyxact.ipyxact import Component
+
+from mako.lookup import TemplateLookup
+from mako.template import Template
+from mako.runtime import Context
+
+import argparse
+
+import yaml
+
+
+
+
+parser = argparse.ArgumentParser(description='Generate Verilog RTL for Config/Status-Register')
+parser.add_argument('xml_path', metavar='<ipcore.xml>', type=file, nargs=1,
+                    help='path to IP-XACT xml-file to be parse')
+args = parser.parse_args()
+inputname = os.path.splitext(args.xml_path[0].name)[0] + ".yml"
+
+try:
+  with open(inputname, 'r') as configfile:
+    cfg = yaml.load(configfile)
+except IOError:
+  with open("config.yml", 'r') as configfile:
+    cfg = yaml.load(configfile)
+
+'''debug print of config sections
+for section in cfg:
+    print(section)
+'''
+clk = str(cfg['General']['Clock'])
+rst = str(cfg['General']['Reset'])
+rst_level   = int(cfg['General']['ResetActiveLevel'])
+rst_is_sync = int(cfg['General']['ResetIsSync'])
+
+
+
+
+component = Component()
+component.load(args.xml_path[0].name)
+
+addressBlock = component.memoryMaps.memoryMap[0].addressBlock[0]
+busByteWidth = component.memoryMaps.memoryMap[0].addressBlock[0].width / 8
+busBitWidth  = component.memoryMaps.memoryMap[0].addressBlock[0].width
+
+fileName = component.name.lower() + '_table_regs_ov.adoc'
+
+
+
+lookup = TemplateLookup(directories=['templates'],
+                        input_encoding='utf-8',
+                        output_encoding='utf-8',
+                        default_filters=['decode.utf8'],
+                        encoding_errors='replace')
+
+buffer = open(fileName, 'w')
+
+buffer.write(""".Register Overview
+[cols="^2e,^2s,32*^1,^3",options="header"]
+|===================================================
+| ADDR | REG |31|30|29|28|27|26|25|24|23|22|21|20|19|18|17|16|15|14|13|12|11|10|9|8|7|6|5|4|3|2|1|0|RST""")
+
+for reg in addressBlock.register:
+    template = lookup.get_template('adoc_regs_overview.mako')
+    ctx = Context(buffer,
+    	          reg = reg,
+    	          cfg = cfg,
+    	          addr_width = component.memoryMaps.memoryMap[0].addressBlock[0].width,
+    	          data_width = component.memoryMaps.memoryMap[0].addressBlock[0].register[0].size)
+    template.render_context(ctx)
+
+buffer.write("\n|===================================================\n")
---
+++ templates/adoc_regs_overview.mako
@@ -0,0 +1,44 @@
+<%
+reg_name = reg.name.upper()
+AssemblyReg = ['RSVD' for i in range(32)]
+
+hex_addr = '0x%02X' % reg.addressOffset
+#hex_addr = "0x{:02X}".format(reg.addressOffset)
+
+for field in reg.field:
+  name = field.name.upper()
+  size = field.bitWidth - 1
+  lsb  = field.bitOffset
+  msb  = field.bitOffset + field.bitWidth - 1
+
+  for i in range(lsb, msb+1):
+    AssemblyReg[i] = str(field.bitWidth) + '+^| ' + field.name + ' '
+
+#  try:
+#    reset_value = 0x'%08X' % (field.resets.reset.value)
+#  except AttributeError:
+#    reset_value = 0xUUUUUUUU
+
+%>\
+<%
+total = ''
+tmp_name = 'null'
+rsvd_count = 0
+
+for i in reversed(range(0, 32)):
+  rsvd_count += 1 if AssemblyReg[i] == 'RSVD' else 0
+  if AssemblyReg[i] != tmp_name:
+    tmp_name = AssemblyReg[i]
+    if AssemblyReg[i] != 'RSVD':
+      if rsvd_count>0:
+        total += str(rsvd_count) + '+^| '
+        rsvd_count = 0
+      total += AssemblyReg[i]
+  if AssemblyReg[i] == 'RSVD' and i == 0:
+    total += str(rsvd_count) + '+^| '
+
+total = total[:-1]
+
+%>\
+
+| ${hex_addr} | ${reg.name}  ${total} | {reset_value}
